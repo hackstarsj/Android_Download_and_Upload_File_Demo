3a694c12d8856eb2619b7d8686514ca114b14de2
sanjeev kumar <sksanjeev362@gmail.com>
2019-10-13 15:30:23 +0530

    Added File Upload Feature

--- app/src/main/java/com/example/androiddownloadmanagerdemo/MainActivity.java
+++ app/src/main/java/com/example/androiddownloadmanagerdemo/MainActivity.java
@@ -1,23 +1,54 @@
 package com.example.androiddownloadmanagerdemo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private static final int REQUEST_GALLERY = 200;
     private long downloadId;
+
+    TextView file_name;
+    String file_path=null;
+    Button upload;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +63,49 @@ public class MainActivity extends AppCompatActivity {
                 beginDownload("http://www.tutorialspoint.com/java/java_tutorial.pdf");
             }
         });
+
+        Button upload_file=findViewById(R.id.upload_file);
+        upload_file.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check permission greater than equal to marshmeellow we used run time permission
+                if(Build.VERSION.SDK_INT>=23){
+                    if(checkPermission()){
+                        filePicker();
+                    }
+                    else{
+                        requestPermission();
+                    }
+                }
+                else{
+                    filePicker();
+                }
+            }
+        });
+
+        progressBar=findViewById(R.id.progress);
+        upload=findViewById(R.id.upload);
+        file_name=findViewById(R.id.filename);
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(file_path!=null){
+                    UploadFile();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Please Select File First", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    private void UploadFile() {
+        UploadTask uploadTask=new UploadTask();
+        uploadTask.execute(new String[]{file_path});
+
     }
 
     private void beginDownload(String file_link){
@@ -63,6 +137,17 @@ public class MainActivity extends AppCompatActivity {
         DownloadManager downloadManager=(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
         downloadId=downloadManager.enqueue(request);
 
+
+    }
+
+    private void filePicker(){
+
+        //.Now Permission Working
+        Toast.makeText(MainActivity.this, "File Picker Call", Toast.LENGTH_SHORT).show();
+        //Let's Pick File
+        Intent opengallery=new Intent(Intent.ACTION_PICK);
+        opengallery.setType("image/*");
+        startActivityForResult(opengallery,REQUEST_GALLERY);
     }
 
     //now checking if download complete
@@ -82,4 +167,132 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         unregisterReceiver(onDownloadComplete);
     }
+
+    private void requestPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+            Toast.makeText(MainActivity.this, "Please Give Permission to Upload File", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private boolean checkPermission(){
+        int result= ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(result== PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_REQUEST_CODE:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this, "Permission Successfull", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Permission Failed", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_GALLERY && resultCode== Activity.RESULT_OK){
+            String filePath=getRealPathFromUri(data.getData(),MainActivity.this);
+            Log.d("File Path : "," "+filePath);
+            //now we will upload the file
+            //lets import okhttp first
+            this.file_path=filePath;
+
+            File file=new File(filePath);
+            file_name.setText(file.getName());
+
+        }
+    }
+
+    public String getRealPathFromUri(Uri uri,Activity activity){
+        Cursor cursor=activity.getContentResolver().query(uri,null,null,null,null);
+        if(cursor==null){
+            return uri.getPath();
+        }
+        else{
+            cursor.moveToFirst();
+            int id=cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(id);
+        }
+    }
+
+
+    public class UploadTask extends AsyncTask<String,String,String>{
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressBar.setVisibility(View.GONE);
+            if(s.equalsIgnoreCase("true")){
+                Toast.makeText(MainActivity.this, "File uploaded", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Failed Upload", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            if(uploadFile(strings[0])){
+                return "true";
+            }
+            else{
+                return "failed";
+            }
+        }
+
+        private boolean uploadFile(String path){
+            File file=new File(path);
+            try{
+                RequestBody requestBody=new MultipartBody.Builder().setType(MultipartBody.FORM)
+                        .addFormDataPart("files",file.getName(),RequestBody.create(MediaType.parse("image/*"),file))
+                        .addFormDataPart("some_key","some_value")
+                        .addFormDataPart("submit","submit")
+                        .build();
+
+                Request request=new Request.Builder()
+                        .url("http://192.168.0.2/project/upload.php")
+                        .post(requestBody)
+                        .build();
+
+                OkHttpClient client = new OkHttpClient();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+
+                    }
+                });
+                return true;
+            }
+            catch (Exception e){
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
+
 }
